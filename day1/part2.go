05c1f95cc25294a66da2dc3a5b3c59e9518c364b
Casey Knolla <cknolla@gmail.com>
2023-12-01 07:47:52 -0800

    part 2 correct answer tested

--- day1/part2.go
+++ day1/part2.go
@@ -1,1 +1,53 @@
 package day1
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var strMap = map[string]string{
+	"zero":  "0",
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+func getPart2CalibrationValue(inputLine string) int {
+	var numbers []string
+	for index, char := range inputLine {
+		if unicode.IsNumber(char) {
+			numbers = append(numbers, string(char))
+			continue
+		}
+		for str, num := range strMap {
+			if strings.HasPrefix(inputLine[index:], str) {
+				numbers = append(numbers, num)
+				continue
+			}
+		}
+	}
+	calibrationValue, err := strconv.Atoi(numbers[0] + numbers[len(numbers)-1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return calibrationValue
+}
+
+func Part2() (string, error) {
+	lines := utils.GetFileLines("part2.txt")
+	sum := 0
+	for _, line := range lines {
+		sum += getPart2CalibrationValue(line)
+	}
+	return fmt.Sprintf("%d", sum), nil
+}
--- day1/part2_test.go
+++ day1/part2_test.go
@@ -1,1 +1,56 @@
 package day1
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetPart2CalibrationValue(t *testing.T) {
+	testCases := []struct {
+		description   string
+		inputLine     string
+		expectedValue int
+	}{
+		{
+			description:   "double integer",
+			inputLine:     "27six2sixjqctbbfv5",
+			expectedValue: 25,
+		},
+		{
+			description:   "double string",
+			inputLine:     "fourfive5jhgpcxmqpr41two",
+			expectedValue: 42,
+		},
+		{
+			description:   "string first integer second",
+			inputLine:     "six1pcknpvv",
+			expectedValue: 61,
+		},
+		{
+			description:   "integer first string second",
+			inputLine:     "5tkvc5twosixspzhdfourninestpsj",
+			expectedValue: 59,
+		},
+		{
+			description:   "single integer",
+			inputLine:     "ptcfnjgchx1",
+			expectedValue: 11,
+		},
+		{
+			description:   "single string",
+			inputLine:     "ptcfivefnjgchx",
+			expectedValue: 55,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			assert.Equal(t, testCase.expectedValue, getPart2CalibrationValue(testCase.inputLine))
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	answer, err := Part2()
+	assert.Nil(t, err)
+	assert.Equal(t, "53855", answer)
+}
