5ba509955053d628051e9cdb6f35ac799426b946
Casey Knolla <cknolla@gmail.com>
2023-12-02 03:18:14 -0800

    day2 part1 correct

--- day2/part1.go
+++ day2/part1.go
@@ -1,1 +1,97 @@
 package day2
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type round struct {
+	red   int
+	green int
+	blue  int
+}
+
+func NewRound() round {
+	return round{
+		red:   0,
+		green: 0,
+		blue:  0,
+	}
+}
+
+type game struct {
+	id     int
+	rounds []round
+}
+
+func parseGame(line string) game {
+	var err error
+	g := game{}
+	majorParts := strings.Split(line, ":")
+	gameParts := strings.Split(majorParts[0], " ")
+	g.id, err = strconv.Atoi(gameParts[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	roundParts := strings.Split(majorParts[1], ";")
+	for _, roundPart := range roundParts {
+		r := NewRound()
+		cubes := strings.Split(roundPart, ",")
+		for _, cube := range cubes {
+			cubeParts := strings.Split(strings.Trim(cube, " "), " ")
+			value, err := strconv.Atoi(cubeParts[0])
+			if err != nil {
+				log.Fatalln(err)
+			}
+			switch cubeParts[1] {
+			case "red":
+				r.red = value
+			case "green":
+				r.green = value
+			case "blue":
+				r.blue = value
+			}
+		}
+		g.rounds = append(g.rounds, r)
+	}
+	return g
+}
+
+func (g *game) roundMaximum() round {
+	maxCubes := round{
+		red:   0,
+		green: 0,
+		blue:  0,
+	}
+	for _, r := range g.rounds {
+		maxCubes.red = int(math.Max(float64(maxCubes.red), float64(r.red)))
+		maxCubes.green = int(math.Max(float64(maxCubes.green), float64(r.green)))
+		maxCubes.blue = int(math.Max(float64(maxCubes.blue), float64(r.blue)))
+	}
+	return maxCubes
+}
+
+func (g *game) isPossible(limit *round) bool {
+	roundMax := g.roundMaximum()
+	return limit.red >= roundMax.red && limit.green >= roundMax.green && limit.blue >= roundMax.blue
+}
+
+func Part1() (string, error) {
+	sum := 0
+	limit := round{
+		red:   12,
+		green: 13,
+		blue:  14,
+	}
+	for line := range utils.GetInputLines() {
+		g := parseGame(line)
+		if g.isPossible(&limit) {
+			sum += g.id
+		}
+	}
+	return fmt.Sprintf("%d", sum), nil
+}
--- day2/part1_test.go
+++ day2/part1_test.go
@@ -1,1 +1,120 @@
 package day2
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseGame(t *testing.T) {
+	g := parseGame("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
+	assert.Equal(t, 3, g.id)
+	assert.Equal(t, []round{
+		{
+			red:   20,
+			green: 8,
+			blue:  6,
+		},
+		{
+			red:   4,
+			green: 13,
+			blue:  5,
+		},
+		{
+			red:   1,
+			green: 5,
+			blue:  0,
+		},
+	}, g.rounds)
+}
+
+func TestRoundMaximum(t *testing.T) {
+	testCases := []struct {
+		description   string
+		inputLine     string
+		expectedValue round
+	}{
+		{
+			description: "Game 1",
+			inputLine:   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			expectedValue: round{
+				red:   4,
+				green: 2,
+				blue:  6,
+			},
+		},
+		{
+			description: "Game 2",
+			inputLine:   "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			expectedValue: round{
+				red:   1,
+				green: 3,
+				blue:  4,
+			},
+		},
+		{
+			description: "Game 3",
+			inputLine:   "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			expectedValue: round{
+				red:   20,
+				green: 13,
+				blue:  6,
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			g := parseGame(testCase.inputLine)
+			assert.Equal(t, testCase.expectedValue, g.roundMaximum())
+		})
+	}
+}
+
+func TestIsPossible(t *testing.T) {
+	testCases := []struct {
+		description   string
+		inputLine     string
+		expectedValue bool
+	}{
+		{
+			description:   "Game 1",
+			inputLine:     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			expectedValue: true,
+		},
+		{
+			description:   "Game 2",
+			inputLine:     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			expectedValue: true,
+		},
+		{
+			description:   "Game 3",
+			inputLine:     "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			expectedValue: false,
+		},
+		{
+			description:   "Game 4",
+			inputLine:     "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+			expectedValue: false,
+		},
+		{
+			description:   "Game 5",
+			inputLine:     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+			expectedValue: true,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			g := parseGame(testCase.inputLine)
+			assert.Equal(t, testCase.expectedValue, g.isPossible(&round{
+				red:   12,
+				green: 13,
+				blue:  14,
+			}))
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	answer, err := Part1()
+	assert.Nil(t, err)
+	assert.Equal(t, "2727", answer)
+}
