34b480a5954fb384fb8a71807d4dee3c1054f8ac
Casey Knolla <cknolla@gmail.com>
2023-12-03 21:27:52 -0800

    d4p1 correct

--- day4/part1.go
+++ day4/part1.go
@@ -1,1 +1,53 @@
 package day4
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"math"
+	"strings"
+)
+
+type card struct {
+	winningNumbers   map[string]bool
+	availableNumbers []string
+}
+
+func newCard() card {
+	return card{
+		winningNumbers: make(map[string]bool),
+	}
+}
+
+func parseLine(line string) card {
+	c := newCard()
+	cardContent := strings.Split(line, ":")[1]
+	numbers := strings.Split(cardContent, "|")
+	for _, winningNumberStr := range strings.Fields(numbers[0]) {
+		c.winningNumbers[winningNumberStr] = true
+	}
+	c.availableNumbers = strings.Fields(numbers[1])
+	return c
+}
+
+func (c *card) getWinningCount() int {
+	winningCount := 0
+	for _, num := range c.availableNumbers {
+		if c.winningNumbers[num] {
+			winningCount += 1
+		}
+	}
+	return winningCount
+}
+
+func Part1() (string, error) {
+	sum := 0
+	for line := range utils.GetInputLines() {
+		c := parseLine(line)
+		winningCount := c.getWinningCount()
+		if winningCount == 0 {
+			continue
+		}
+		sum += int(math.Pow(2, float64(c.getWinningCount()-1)))
+	}
+	return fmt.Sprintf("%d", sum), nil
+}
--- day4/part1_test.go
+++ day4/part1_test.go
@@ -1,1 +1,73 @@
 package day4
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseLine(t *testing.T) {
+	testCases := []struct {
+		description  string
+		inputLine    string
+		expectedCard card
+	}{
+		{
+			description: "Card 1",
+			inputLine:   "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+			expectedCard: card{
+				winningNumbers: map[string]bool{
+					"41": true,
+					"48": true,
+					"83": true,
+					"86": true,
+					"17": true,
+				},
+				availableNumbers: []string{
+					"83", "86", "6", "31", "17", "9", "48", "53",
+				},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			c := parseLine(testCase.inputLine)
+			assert.Equal(t, testCase.expectedCard, c)
+		})
+	}
+}
+
+func TestGetWinningCount(t *testing.T) {
+	testCases := []struct {
+		description        string
+		card               card
+		expectedPowerOfTwo int
+	}{
+		{
+			description: "Card 1",
+			card: card{
+				winningNumbers: map[string]bool{
+					"41": true,
+					"48": true,
+					"83": true,
+					"86": true,
+					"17": true,
+				},
+				availableNumbers: []string{
+					"83", "86", "6", "31", "17", "9", "48", "53",
+				},
+			},
+			expectedPowerOfTwo: 4,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			assert.Equal(t, testCase.expectedPowerOfTwo, testCase.card.getWinningCount())
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	answer, err := Part1()
+	assert.Nil(t, err)
+	assert.Equal(t, "19855", answer)
+}
