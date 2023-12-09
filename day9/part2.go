a0d6640aeb9cf07bc4abc3415482b09684833e78
Casey Knolla <cknolla@gmail.com>
2023-12-09 03:15:02 -0800

    d9p2 correct

--- day9/part2.go
+++ day9/part2.go
@@ -1,1 +1,23 @@
 package day9
+
+import "fmt"
+
+func (h *history) getPrevValue() int {
+	if len(h.values) == 1 {
+		h.buildDifferences(h.values[0])
+	}
+	diff := 0
+	for i := len(h.values) - 2; i >= 0; i-- {
+		diff = h.values[i][0] - diff
+	}
+	return diff
+}
+
+func Part2() (string, error) {
+	answer := 0
+	histories := parseFile("input.txt")
+	for _, history := range histories {
+		answer += history.getPrevValue()
+	}
+	return fmt.Sprintf("%d", answer), nil
+}
--- day9/part2_test.go
+++ day9/part2_test.go
@@ -1,1 +1,22 @@
 package day9
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetPrevValue(t *testing.T) {
+	histories := parseFile("sample.txt")
+	nextValue := histories[0].getPrevValue()
+	assert.Equal(t, -3, nextValue)
+	nextValue = histories[1].getPrevValue()
+	assert.Equal(t, 0, nextValue)
+	nextValue = histories[2].getPrevValue()
+	assert.Equal(t, 5, nextValue)
+}
+
+func TestPart2(t *testing.T) {
+	answer, err := Part2()
+	assert.Nil(t, err)
+	assert.Equal(t, "1208", answer)
+}
