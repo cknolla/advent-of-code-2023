9b75539f9fb536e7f283840e973289af3ec1d32e
Casey Knolla <cknolla@gmail.com>
2023-12-09 03:09:46 -0800

    d9p1 correct

--- day9/part1.go
+++ day9/part1.go
@@ -1,1 +1,64 @@
 package day9
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type history struct {
+	values [][]int
+}
+
+func (h *history) getNextValue() int {
+	if len(h.values) == 1 {
+		h.buildDifferences(h.values[0])
+	}
+	diff := 0
+	for i := len(h.values) - 2; i >= 0; i-- {
+		diff = diff + h.values[i][len(h.values[i])-1]
+	}
+	return diff
+}
+
+func (h *history) buildDifferences(values []int) {
+	difference := make([]int, len(values)-1)
+	nonZero := false
+	for i := 0; i < len(values)-1; i++ {
+		difference[i] = values[i+1] - values[i]
+		if difference[i] != 0 {
+			nonZero = true
+		}
+	}
+	h.values = append(h.values, difference)
+	if nonZero {
+		h.buildDifferences(difference)
+	}
+}
+
+func parseFile(filename string) []history {
+	histories := make([]history, 200)
+	for l := range utils.GetInputLines(filename) {
+		line := l.Text
+		if line == "" {
+			continue
+		}
+		strValues := strings.Fields(line)
+		histories[l.Index].values = append(histories[l.Index].values, make([]int, len(strValues)))
+		for i, strValue := range strValues {
+			intValue, _ := strconv.Atoi(strValue)
+			histories[l.Index].values[0][i] = intValue
+		}
+	}
+	return histories
+}
+
+func Part1() (string, error) {
+	answer := 0
+	histories := parseFile("input.txt")
+	for _, history := range histories {
+		answer += history.getNextValue()
+	}
+	return fmt.Sprintf("%d", answer), nil
+}
--- day9/part1_test.go
+++ day9/part1_test.go
@@ -1,1 +1,50 @@
 package day9
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	histories := parseFile("sample.txt")
+	assert.Equal(t, history{[][]int{{0, 3, 6, 9, 12, 15}}}, histories[0])
+	assert.Equal(t, history{[][]int{{1, 3, 6, 10, 15, 21}}}, histories[1])
+	assert.Equal(t, history{[][]int{{10, 13, 16, 21, 30, 45}}}, histories[2])
+}
+
+func TestBuildDifferences(t *testing.T) {
+	histories := parseFile("sample.txt")
+	histories[0].buildDifferences(histories[0].values[0])
+	assert.Equal(t, history{
+		[][]int{
+			{0, 3, 6, 9, 12, 15},
+			{3, 3, 3, 3, 3},
+			{0, 0, 0, 0},
+		},
+	}, histories[0])
+	histories[1].buildDifferences(histories[1].values[0])
+	assert.Equal(t, history{
+		[][]int{
+			{1, 3, 6, 10, 15, 21},
+			{2, 3, 4, 5, 6},
+			{1, 1, 1, 1},
+			{0, 0, 0},
+		},
+	}, histories[1])
+}
+
+func TestGetNextValue(t *testing.T) {
+	histories := parseFile("sample.txt")
+	nextValue := histories[0].getNextValue()
+	assert.Equal(t, 18, nextValue)
+	nextValue = histories[1].getNextValue()
+	assert.Equal(t, 28, nextValue)
+	nextValue = histories[2].getNextValue()
+	assert.Equal(t, 68, nextValue)
+}
+
+func TestPart1(t *testing.T) {
+	answer, err := Part1()
+	assert.Nil(t, err)
+	assert.Equal(t, "2174807968", answer)
+}
