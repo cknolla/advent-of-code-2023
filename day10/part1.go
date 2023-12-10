9d20f76919617c444a8b25d7e827dabd12c4e53e
Casey Knolla <cknolla@gmail.com>
2023-12-10 06:57:21 -0800

    d10p1 correct

--- day10/part1.go
+++ day10/part1.go
@@ -1,1 +1,98 @@
 package day10
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"strings"
+)
+
+type maze struct {
+	runes       [][]rune
+	startingLoc location
+	rowCount    int
+	colCount    int
+}
+
+type location struct {
+	y int
+	x int
+}
+
+func (l *location) Add(direction *location) {
+	l.x += direction.x
+	l.y += direction.y
+}
+
+func (l *location) nextNeighbor(r rune, prevLoc *location) location {
+	directions := directionMap[r]
+	if l.y+directions[0].y == prevLoc.y && l.x+directions[0].x == prevLoc.x {
+		return location{l.y + directions[1].y, l.x + directions[1].x}
+	} else {
+		return location{l.y + directions[0].y, l.x + directions[0].x}
+	}
+}
+
+var directionMap = map[rune][]location{
+	'-': {{0, -1}, {0, 1}},
+	'|': {{-1, 0}, {1, 0}},
+	'L': {{0, 1}, {-1, 0}},
+	'7': {{0, -1}, {1, 0}},
+	'J': {{0, -1}, {-1, 0}},
+	'F': {{0, 1}, {1, 0}},
+}
+
+// from starting traverse maze and return total steps taken
+func (m *maze) traverse() (steps int) {
+	currentLoc := m.startingLoc
+	prevLoc := currentLoc
+	// first order of business is finding out which way to go from start
+	if currentLoc.x != 0 && strings.ContainsRune("-LF", m.runes[currentLoc.y][currentLoc.x-1]) {
+		currentLoc.Add(&location{0, -1})
+	} else if currentLoc.y != 0 && strings.ContainsRune("|LJ", m.runes[currentLoc.y-1][currentLoc.x]) {
+		currentLoc.Add(&location{-1, 0})
+	} else if currentLoc.x != m.rowCount-1 && strings.ContainsRune("-7J", m.runes[currentLoc.y][currentLoc.x+1]) {
+		currentLoc.Add(&location{0, 1})
+	} else {
+		currentLoc.Add(&location{1, 0})
+	}
+	steps++
+	// then continue finding other neighbor until encountering starting pos again
+	for m.runes[currentLoc.y][currentLoc.x] != 'S' {
+		nextLoc := currentLoc.nextNeighbor(m.runes[currentLoc.y][currentLoc.x], &prevLoc)
+		prevLoc = currentLoc
+		currentLoc = nextLoc
+		steps++
+	}
+	return steps
+}
+
+func parseFile(filename string) maze {
+	var m maze
+	m.runes = make([][]rune, 140)
+	for i := range m.runes {
+		m.runes[i] = make([]rune, 140)
+	}
+	for l := range utils.GetInputLines(filename) {
+		line := l.Text
+		if line == "" {
+			continue
+		}
+		for x, r := range l.Text {
+			m.runes[l.Index][x] = r
+			if r == 'S' {
+				m.startingLoc = location{l.Index, x}
+			}
+			if m.rowCount == 0 {
+				m.colCount++
+			}
+		}
+		m.rowCount++
+	}
+	return m
+}
+
+func Part1() (string, error) {
+	maze := parseFile("input.txt")
+	steps := maze.traverse()
+	return fmt.Sprintf("%d", steps/2), nil
+}
--- day10/part1_test.go
+++ day10/part1_test.go
@@ -1,1 +1,18 @@
 package day10
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	maze := parseFile("sample.txt")
+	assert.Equal(t, location{2, 0}, maze.startingLoc)
+	assert.Equal(t, []rune{'.', 'F', 'J', '|', '7'}, maze.runes[1][0:5])
+}
+
+func TestPart1(t *testing.T) {
+	answer, err := Part1()
+	assert.Nil(t, err)
+	assert.Equal(t, "6860", answer)
+}
