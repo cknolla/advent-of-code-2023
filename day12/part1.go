2fe751c53423369ec20680ce167ace918b1d7f0d
Casey Knolla <cknolla@gmail.com>
2023-12-12 16:40:46 -0800

    d12p1 correct

--- day12/part1.go
+++ day12/part1.go
@@ -1,1 +1,89 @@
 package day12
+
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type row struct {
+	springs  []rune
+	criteria []int
+}
+
+func (r *row) matchesCriteria() bool {
+	contiguousSprings := 0
+	var springCounts []int
+	for _, spring := range r.springs {
+		if spring == '.' {
+			if contiguousSprings > 0 {
+				springCounts = append(springCounts, contiguousSprings)
+			}
+			contiguousSprings = 0
+		} else if spring == '#' {
+			contiguousSprings++
+		}
+	}
+	if contiguousSprings > 0 {
+		springCounts = append(springCounts, contiguousSprings)
+	}
+	if slices.Equal(springCounts, r.criteria) {
+		return true
+	}
+	return false
+}
+
+func (r *row) testAll() int {
+	var indexes []int
+	for index, spring := range r.springs {
+		if spring == '?' {
+			indexes = append(indexes, index)
+		}
+	}
+	unknownCount := len(indexes)
+	validArrangements := 0
+	for i := 0; i < int(math.Pow(float64(2), float64(unknownCount))); i++ {
+		for j := 0; j < unknownCount; j++ {
+			if i&(1<<j) == 0 {
+				r.springs[indexes[j]] = '.'
+			} else {
+				r.springs[indexes[j]] = '#'
+			}
+		}
+		if r.matchesCriteria() {
+			validArrangements++
+		}
+	}
+	return validArrangements
+}
+
+func parseFile(filename string) []row {
+	rows := make([]row, 1000)
+	for l := range utils.GetInputLines(filename) {
+		line := l.Text
+		if line == "" {
+			continue
+		}
+		segments := strings.Fields(l.Text)
+		rows[l.Index].springs = []rune(segments[0])
+		criteraStrs := strings.Split(segments[1], ",")
+		for _, c := range criteraStrs {
+			criteriaInt, _ := strconv.Atoi(c)
+			rows[l.Index].criteria = append(rows[l.Index].criteria, criteriaInt)
+		}
+	}
+	return rows
+}
+
+func Part1() (string, error) {
+	sum := 0
+	rows := parseFile("input.txt")
+	for _, row := range rows {
+		validArrangements := row.testAll()
+		sum += validArrangements
+	}
+	return fmt.Sprintf("%d", sum), nil
+}
--- day12/part1_test.go
+++ day12/part1_test.go
@@ -1,1 +1,44 @@
 package day12
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	rows := parseFile("input.txt")
+	assert.Equal(t, []rune{'?', '?', '?', '?', '?', '?', '#', '?', '?', '#', '?', '?'}, rows[0].springs)
+	assert.Equal(t, []int{1, 1, 5, 1}, rows[0].criteria)
+	assert.Equal(t, []rune("??.?????#?"), rows[999].springs)
+	assert.Equal(t, []int{1, 5}, rows[999].criteria)
+}
+
+func TestMatchesCriteria(t *testing.T) {
+	r := row{[]rune("?###????????"), []int{3, 2, 1}}
+	r.springs = []rune(".###.##.#...")
+	assert.True(t, r.matchesCriteria())
+	r.springs = []rune(".###.##....#")
+	assert.True(t, r.matchesCriteria())
+	r.springs = []rune(".######.#...")
+	assert.False(t, r.matchesCriteria())
+	r.springs = []rune(".###.##.#.#.")
+	assert.False(t, r.matchesCriteria())
+	r.springs = []rune(".###.##.##..")
+	assert.False(t, r.matchesCriteria())
+}
+
+func TestTestAll(t *testing.T) {
+	rows := parseFile("sample.txt")
+	sum := 0
+	for i := 0; i < 6; i++ {
+		validArrangements := rows[i].testAll()
+		sum += validArrangements
+	}
+	assert.Equal(t, 21, sum)
+}
+
+func TestPart1(t *testing.T) {
+	answer, err := Part1()
+	assert.Nil(t, err)
+	assert.Equal(t, "6488", answer)
+}
