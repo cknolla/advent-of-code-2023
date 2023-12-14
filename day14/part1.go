557536c91562e33e58d8d6ec68ae31eec7820716
Casey Knolla <cknolla@gmail.com>
2023-12-13 22:23:50 -0800

    d14p1 correct

--- day14/part1.go
+++ day14/part1.go
@@ -1,1 +1,85 @@
 package day14
+
+import "advent-of-code-2023/utils"
+
+const (
+	SPACE = iota
+	ROUND_ROCK
+	CUBE_ROCK
+)
+
+const (
+	NORTH = iota
+	EAST
+	SOUTH
+	WEST
+)
+
+//type location struct {
+//	occupant int
+//
+//}
+
+func tilt(locations [][]int, direction int) [][]int {
+	for rowIndex := 0; rowIndex < len(locations); rowIndex++ {
+		switch direction {
+		case NORTH:
+			for colIndex := 0; colIndex < len(locations[rowIndex]); colIndex++ {
+				if locations[rowIndex][colIndex] != ROUND_ROCK {
+					continue
+				}
+				for index := rowIndex; index > 0; index-- {
+					if locations[index-1][colIndex] == SPACE {
+						locations[index-1][colIndex] = ROUND_ROCK
+						locations[index][colIndex] = SPACE
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+	return locations
+}
+
+func calculateLoad(locations *[][]int) (totalLoad int) {
+	for rowIndex, location := range *locations {
+		for _, object := range location {
+			if object == ROUND_ROCK {
+				totalLoad += len(*locations) - rowIndex
+			}
+		}
+	}
+	return totalLoad
+}
+
+func parseFile(filename string) [][]int {
+	var locations [][]int
+	for l := range utils.GetInputLines(filename) {
+		line := l.Text
+		if line == "" {
+			continue
+		}
+		row := make([]int, len(line))
+		for index, char := range line {
+			switch char {
+			case '.':
+				row[index] = SPACE
+			case 'O':
+				row[index] = ROUND_ROCK
+			default:
+				row[index] = CUBE_ROCK
+			}
+		}
+		locations = append(locations, row)
+	}
+	return locations
+}
+
+func Part1(filename string) int {
+	answer := 0
+	locations := parseFile(filename)
+	locations = tilt(locations, NORTH)
+	answer = calculateLoad(&locations)
+	return answer
+}
--- day14/part1_test.go
+++ day14/part1_test.go
@@ -1,1 +1,31 @@
 package day14
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	locations := parseFile("sample.txt")
+	assert.Equal(t, []int{ROUND_ROCK, SPACE, SPACE, SPACE, SPACE, CUBE_ROCK, SPACE, SPACE, SPACE, SPACE}, locations[0])
+	assert.Equal(t, []int{ROUND_ROCK, SPACE, CUBE_ROCK, SPACE, SPACE, ROUND_ROCK, SPACE, CUBE_ROCK, SPACE, CUBE_ROCK}, locations[5])
+	assert.Equal(t, []int{CUBE_ROCK, ROUND_ROCK, ROUND_ROCK, SPACE, SPACE, CUBE_ROCK, SPACE, SPACE, SPACE, SPACE}, locations[9])
+}
+
+func TestTilt(t *testing.T) {
+	locations := parseFile("sample.txt")
+	locations = tilt(locations, NORTH)
+	assert.Equal(t, []int{ROUND_ROCK, ROUND_ROCK, ROUND_ROCK, ROUND_ROCK, SPACE, CUBE_ROCK, SPACE, ROUND_ROCK, SPACE, SPACE}, locations[0])
+	assert.Equal(t, []int{ROUND_ROCK, ROUND_ROCK, SPACE, SPACE, CUBE_ROCK, SPACE, SPACE, SPACE, SPACE, CUBE_ROCK}, locations[1])
+	assert.Equal(t, []int{CUBE_ROCK, SPACE, SPACE, SPACE, SPACE, CUBE_ROCK, SPACE, SPACE, SPACE, SPACE}, locations[9])
+}
+
+func TestPart1Sample(t *testing.T) {
+	answer := Part1("sample.txt")
+	assert.Equal(t, 136, answer)
+}
+
+func TestPart1(t *testing.T) {
+	answer := Part1("input.txt")
+	assert.Equal(t, 108889, answer)
+}
